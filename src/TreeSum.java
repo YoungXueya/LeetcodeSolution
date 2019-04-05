864a3352a5f52e646d781c2ad801c7bf7af86355
youngxueya <YoungXueya@gmail.com>
2019-04-06 00:22:36 +0800

    15.ThreeSum

---
+++ src/TreeSum.java
@@ -0,0 +1,90 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res=new ArrayList();
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-2;i++){
+            if(i>0&&nums[i]==nums[i-1]) continue;
+            int lo=i+1,hi=nums.length-1,target=0-nums[i];
+            while(lo<hi){
+                if(nums[lo]+nums[hi]==target){
+                    res.add(Arrays.asList(nums[i],nums[lo],nums[hi]));
+                    lo++;
+                    hi--;
+                    while(lo<hi&&nums[lo]==nums[lo-1]) lo++;
+                    while(lo<hi&&nums[hi]==nums[hi+1]) hi--;
+
+                }
+                else if(nums[lo]+nums[hi]<target) lo++;
+                else hi--;
+            }
+        }
+        return res;
+    }
+}
+
+public class TreeSum {
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return new int[0];
+        }
+
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for(int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+
+    public static String integerArrayListToString(List<Integer> nums, int length) {
+        if (length == 0) {
+            return "[]";
+        }
+
+        String result = "";
+        for(int index = 0; index < length; index++) {
+            Integer number = nums.get(index);
+            result += Integer.toString(number) + ", ";
+        }
+        return "[" + result.substring(0, result.length() - 2) + "]";
+    }
+
+    public static String integerArrayListToString(List<Integer> nums) {
+        return integerArrayListToString(nums, nums.size());
+    }
+
+    public static String int2dListToString(List<List<Integer>> nums) {
+        StringBuilder sb = new StringBuilder("[");
+        for (List<Integer> list: nums) {
+            sb.append(integerArrayListToString(list));
+            sb.append(",");
+        }
+
+        sb.setCharAt(sb.length() - 1, ']');
+        return sb.toString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            int[] nums = stringToIntegerArray(line);
+
+            List<List<Integer>> ret = new Solution().threeSum(nums);
+
+            String out = int2dListToString(ret);
+
+            System.out.print(out);
+        }
+    }
+}
