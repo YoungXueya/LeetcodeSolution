34e65a945250080c682c967b61860531fa677e57
Yang Xueya <YoungXueya@gmail.com>
2019-06-08 21:49:53 +0800

    Create 205. Isomorphic Strings.py

---
+++ src/205. Isomorphic Strings.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        mapping={}
+        for i in range(len(s)):
+            if  s[i] in mapping:
+                if mapping[s[i]]==t[i]:
+                    continue
+                else:
+                    return False
+            else:
+                if t[i] in mapping.values():
+                    return False
+                else:
+                    mapping[s[i]]=t[i]
+        return True
+                
+        
