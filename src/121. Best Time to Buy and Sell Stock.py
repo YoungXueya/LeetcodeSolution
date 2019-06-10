8fe18d0c57bfb98b0a1dd9510b1299be551b55e1
Yang Xueya <YoungXueya@gmail.com>
2019-06-11 00:35:50 +0800

    Create 121. Best Time to Buy and Sell Stock.py

---
+++ src/121. Best Time to Buy and Sell Stock.py
@@ -0,0 +1,14 @@
+class Solution:
+    
+    def maxProfit(self, prices: List[int]) -> int:
+        maxCur,maxSoFar=0,0
+        for i in range(1,len(prices)):
+            maxCur=max(0,maxCur+prices[i]-prices[i-1])
+            maxSoFar=max(maxCur,maxSoFar)
+        return maxSoFar
+    def maxProfit1(self, prices: List[int]) -> int:
+        min_price,profit=float('inf'),0
+        for price in prices:
+            min_price=min(min_price,price)
+            profit=max(profit,price-min_price)
+        return profit
