905f01636a4e8efad9c9aae95c0830a02b59c82c
Yang Xueya <YoungXueya@gmail.com>
2019-06-10 11:37:01 +0800

    Create 100. Same Tree.py

---
+++ 100. Same Tree.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # Good Example of Recursion
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if p==None or q==None:
+            return p==q
+        if p.val==q.val:
+            return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
+        return False
+            
+                
+        
+        
+        
