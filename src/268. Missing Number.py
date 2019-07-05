6adae292d11643778179eada52ff077a670ad626
Yang Xueya <YoungXueya@gmail.com>
2019-07-05 11:43:13 +0800

    Create 268. Missing Number.py

---
+++ src/268. Missing Number.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    
+    def missingNumber(self, nums):
+        Set=set()
+        for item in nums:
+            Set.add(item)
+        for i in range(len(nums)+1):
+            if i not in Set:
+                return i
+            
+    def missingNumber1(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n=len(nums)        
+        return n*(n+1)/2-sum(nums)
+        
