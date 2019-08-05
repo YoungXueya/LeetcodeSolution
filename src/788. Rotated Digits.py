b886af3ed35ad30498574ebf0be0f494f958b2e5
Yang Xueya <hangengainio@163.com>
2019-08-05 21:43:07 +0800

    Update 788. Rotated Digits.py

--- src/788. Rotated Digits.py
+++ src/788. Rotated Digits.py
@@ -1,21 +1,18 @@
 class Solution:
-    def maxDistToClosest(self, seats: List[int]) -> int:
-        Max=-1
-        last=-1
-        flag=True
-        for i in range(len(seats)):
-            if seats[i]==0:
-                if i==len(seats)-1:
-                    print("Here")
-                    Max=max(Max,len(seats)-1-last)
-                continue
-            else:
-                print("last",last)
-                print("Max",Max)
-                if last==-1:
-                    Max=max(i,Max)
-                
+    def rotatedDigits(self, N: int) -> int:
+        dic={"0":"0","1":"1","2":"5","5":"2","6":"9","8":"8","9":"6"}
+        count=0
+        for i in range(1,N+1):
+            isValid=True
+            old=str(i)
+            print(old)
+            new=""
+            for ch in old:
+                if ch not in dic.keys():
+                    isValid=False
+                    break
                 else:
-                    Max=max(Max,(i-last)/2)
-                last=i
-        return int(Max)
+                    new+=dic[ch]
+            if isValid and new !=old:
+                count+=1
+        return count
