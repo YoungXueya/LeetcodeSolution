6c89b5f61dc7687c5f23adea0db0ef38ce01e0bd
Yang Xueya <hangengainio@163.com>
2019-09-11 12:25:19 +0800

    Update 415. Add Strings.py

--- src/415. Add Strings.py
+++ src/415. Add Strings.py
@@ -1,4 +1,5 @@
 class Solution:
+    #
     def addStrings(self, num1: str, num2: str) -> str:
         carry=0
         res=[]
@@ -6,9 +7,7 @@ class Solution:
         
         while i>=0 or j>=0:
             n1 = ord(num1[i])-ord('0') if i>=0 else 0
-            print(n1)
             n2 = ord(num2[j])-ord('0') if j>=0 else 0
-            print(n2)
             i-=1
             j-=1
             
@@ -17,3 +16,16 @@ class Solution:
             carry = temp // 10
         if carry: res.append(carry)
         return ''.join([str(i) for i in res])[::-1]
+        
+    # Cheating solution, because python has no limits
+    def addStrings(self, num1: str, num2: str) -> str:
+        val1=0
+        val2=0
+        for i in num1:
+            val1=val1*10+(ord(i)-ord('0'))
+            
+        for j in num2:
+            val2=val2*10+(ord(j)-ord('0'))
+        return str(val1+val2)
+            
+                
