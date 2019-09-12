0f2ff3de9dd02f824d4cb8257ba9fb52ad2d702b
Yang Xueya <hangengainio@163.com>
2019-09-12 12:24:18 +0800

    Update 1122. Relative Sort Array.py

--- src/1122. Relative Sort Array.py
+++ src/1122. Relative Sort Array.py
@@ -1,4 +1,7 @@
 class Solution:
+    
+    # Initial idea
+    # In-space, but O(n^2) time complexity
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
         slow=0
         for item in arr2:
@@ -27,6 +30,27 @@ class Solution:
         self.quicksort(arr,start,i-1)
         self.quicksort(arr,i+1,end)
         
+    ## Better Solution using count, O(n) time and space complexity and 
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        slow=0
+        dic={}
+        diff=[]
+        for item in arr2:
+            dic[item]=0
+        
+        for j in range(len(arr1)):
+            if arr1[j] in dic:
+                dic[arr1[j]]+=1
+            else:
+                diff.append(arr1[j])
+        
+        diff.sort()
+        count=[]      
+        for item in arr2:
+            count+=[item]*dic[item]
+        count+=diff
+        return count
+    
         
             
         
