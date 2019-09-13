bab5cf78b3bddd7fec0ec77992eed83a1f8b3202
Yang Xueya <hangengainio@163.com>
2019-09-13 23:16:03 +0800

    Update 461. Hamming Distance.py

--- src/461. Hamming Distance.py
+++ src/461. Hamming Distance.py
@@ -4,5 +4,16 @@ class Solution:
         result=bin(x^y).count('1')
        
         return result
+    # x&(x-1) remove the last 1 in the number
+    # It could also used to judge is a number is even:
+    # if( (x&(x-1)) == 0 ) return 1;
+    # else return 0;
+    def hammingDistance(self, x: int, y: int) -> int:
+        x=x^y
+        count=0
+        while x:
+            x=x&(x-1)
+            count+=1
+        return count
     
         
