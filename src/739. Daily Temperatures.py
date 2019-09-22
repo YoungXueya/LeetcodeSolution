281759e0ac73e6007a548d487efb8f40406424bf
Yang Xueya <hangengainio@163.com>
2019-09-22 23:26:51 +0800

    Update 739. Daily Temperatures.py

--- src/739. Daily Temperatures.py
+++ src/739. Daily Temperatures.py
@@ -1,4 +1,8 @@
 class Solution:
+    # https://leetcode.com/problems/daily-temperatures/discuss/121787/C%2B%2B-Clean-code-with-explanation%3A-O(n)-time-and-O(1)-space-(beats-99.13)
+    # Logic is if T[i]<T[i+1], if result[i+1]>0 means that the following result[i+1] is smaller than T[i],
+    # Therefore, compare with elements at least result[i] latter.
+    # End condition of comparison is if result[j]==0, then no other element bigger than T[i] latter.
     def dailyTemperatures(self, T: List[int]) -> List[int]:
         result=[0 for i in range(len(T))]
         count=0
