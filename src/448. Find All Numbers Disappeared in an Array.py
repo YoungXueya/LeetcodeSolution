0daa27352b910ef9d6a1d53d5c20b48945989a53
Yang Xueya <hangengainio@163.com>
2019-09-30 20:53:05 +0800

    Update and rename 448. Find All Numbers Disappeared in an Array.py to src/448. Find All Numbers Disappeared in an Array.py

--- src/448. Find All Numbers Disappeared in an Array.py
+++ src/448. Find All Numbers Disappeared in an Array.py
@@ -1,5 +1,6 @@
 class Solution:
-    # If the number appears, mark it as 
+    # If the number appears, mark it as negative,
+    # For the consideration of overflow, 
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
         for i in range(len(nums)):
             index = abs(nums[i]) - 1
