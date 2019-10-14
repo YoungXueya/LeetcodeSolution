5e365b8a81e5d35f7687889aa6889ce7b608a85e
Yang Xueya <hangengainio@163.com>
2019-10-14 10:57:57 +0800

    Update 55. Jump Game.py

--- src/55. Jump Game.py
+++ src/55. Jump Game.py
@@ -1,4 +1,5 @@
 class Solution:
+    # From start to the end
     def canJump(self, nums: List[int]) -> bool:
         maxStep=0
         for i in range(len(nums)):
@@ -6,4 +7,11 @@ class Solution:
                 return False
             maxStep=max(i+nums[i],maxStep)
         return True
+    # From end to the start 
+    def canJump(self, nums: List[int]) -> bool:
+        lastPos=len(nums)-1
+        for i in range(len(nums)-1,-1,-1):
+            if nums[i]+i>=lastPos:
+                lastPos=i
+        return lastPos==0
         
