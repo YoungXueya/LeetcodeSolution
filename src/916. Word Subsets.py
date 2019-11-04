5f158c128f82775601dbf1552aecf82a2e2d249f
Yang Xueya <hangengainio@163.com>
2019-11-04 22:46:51 +0800

    Create 916. Word Subsets.py

---
+++ src/916. Word Subsets.py
@@ -0,0 +1,12 @@
+## c & d                       # intersection:  min(c[x], d[x]) #
+## c | d                       # union:  max(c[x], d[x])
+
+from collections import Counter
+class Solution:
+    def wordSubsets(self, A: List[str], B: List[str]) -> List[str]:
+        
+        count = Counter()
+        for b in B:
+            count = count | Counter(b)
+        return [a for a in A if Counter(a) & count == count]
+        
